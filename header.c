a16fe67ebd5758102d17a54d24bf0e116e33ba6d
boochow <boochowp@gmail.com>
2024-03-20 13:37:37 +0900

    set the default value for input gain

--- header.c
+++ header.c
@@ -50,7 +50,7 @@ const __unit_header unit_header_t unit_header = {
     .unit_id = 0x01010000,  // Product number(01),Unit type(01=Synth),reserved
     .version = 0x00010000U,
     .name = "PSHFTR",
-    .num_params = 4,
+    .num_params = 5,
     .params = {
         // Format:
         // min, max, center, default, type, frac. bits, frac. mode, <reserved>, name
@@ -59,7 +59,7 @@ const __unit_header unit_header_t unit_header = {
         {0, 1023, 0, 0, k_unit_param_type_none, 0, 0, 0, {"SHPE"}},
 
         // B knob
-        {0, 1023, 0, 0, k_unit_param_type_none, 0, 0, 0, {"ALT"}},
+        {0, 1023, 511, 431, k_unit_param_type_none, 0, 0, 0, {"ALT"}},
 
         // 8 Edit menu parameters
         {0, 8, 0, 0, k_unit_param_type_none, 0, 0, 0, {"SIZE"}},
